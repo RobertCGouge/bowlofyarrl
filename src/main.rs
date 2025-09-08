e434b94758c9de5453b3d035769778e0290f44d0
Gouge, Robert <robert.gouge@cgi.com>
2025-09-08 08:57:53 -0500

    monsters now chase the player

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,49 @@ impl State {
             monster_systems: build_monster_scheduler(),
         }
     }
+
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
+        ctx.print_color_centered(
+            4,
+            WHITE,
+            BLACK,
+            "Slain by a monster, your hero's journey has come to a \
+ 	        premature end.",
+        );
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "The Amulet of Yala remains unclaimed, and your home town \
+ 	        is not saved.",
+        );
+        ctx.print_color_centered(
+            8,
+            YELLOW,
+            BLACK,
+            "Don't worry, you can always try again with a new hero.",
+        );
+        ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play again.");
+
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.ecs = World::default();
+            self.resources = Resources::default();
+            let mut rng = RandomNumberGenerator::new();
+            let map_builder = MapBuilder::new(&mut rng);
+            spawn_player(&mut self.ecs, map_builder.player_start);
+            map_builder
+                .rooms
+                .iter()
+                .skip(1)
+                .map(|r| r.center())
+                .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+            self.resources.insert(map_builder.map);
+            self.resources.insert(Camera::new(map_builder.player_start));
+            self.resources.insert(TurnState::AwaitingInput);
+        }
+    }
 }
 
 impl GameState for State {
@@ -86,6 +129,9 @@ impl GameState for State {
                 self.monster_systems
                     .execute(&mut self.ecs, &mut self.resources);
             }
+            turn_state::TurnState::GameOver => {
+                self.game_over(ctx);
+            }
         }
         render_draw_buffer(ctx).expect("Failed to render draw buffer");
     }
@@ -93,7 +139,7 @@ impl GameState for State {
 
 fn main() -> BError {
     let context = BTermBuilder::new() // (1)
-        .with_title("Dungeon Crawler")
+        .with_title("Bowl of Yarrl")
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // (2)
         .with_tile_dimensions(32, 32) // (3)
